9116ae9aa1bd616eb717dd660245416fb21f49d0
Temerov Anton <hurdlelightsick@gmail.com>
2023-08-03 18:40:42 +0700

    Убрал тень у хедера

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   const { userId } = auth();
 
   return (
-    <header className="fixed z-20 left-20 right-20 top-[60px] flex items-center justify-center xl:justify-between py-6 px-[60px] rounded-[50px] backdrop-brightness-75 backdrop-blur-xl shadow-2xl">
+    <header className="fixed z-20 left-20 right-20 top-[60px] flex items-center justify-center xl:justify-between py-6 px-[60px] rounded-[50px] backdrop-brightness-75 backdrop-blur-xl">
       <Link href="/" className="hidden xl:flex items-center gap-5">
         <Image
           width={35}
